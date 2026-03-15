c181bee028746392e452dbbc7e39444617ae3b0d
Lim Jun Hwa <139619593+BlackButcher04@users.noreply.github.com>
2026-03-15 16:18:15 +0800

    Junhwa - Adding New Pages (#6)
    * Modifiing
    
    * Adding new pages

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,40 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+import joblib    
+    
 # You may start your code here
 st.title("Dashboard Overview")
+
+row1 = st.columns(4)
+
+with row1[0].container(height=160):
+    st.success("Active Machines 🏭")
+    st.subheader("12 Machine(s)")
+    
+with row1[1].container(height=160):
+    st.info("Sensors Monitered 📡")
+    st.subheader("1002 Sensor(s)")
+    
+with row1[2].container(height=160):
+    st.warning("Pending Maintenance ⚙️")
+    st.subheader("1 Pending(s)")
+    
+with row1[3].container(height=160):
+    st.error("Critical Alerts 🚨")
+    st.subheader("1 Alert(s)")
+
+st.subheader("Machines' Location")
+
+# 1. Create a dataset with latitude and longitude 
+# (Dummy coordinates around Kuala Lumpur for your VM test)
+map_data = pd.DataFrame({
+    "Machine ID": ["M-01", "M-02", "M-03"],
+    "lat": [3.1576, 3.1174, 3.1073],   
+    "lon": [101.7116, 101.6775, 101.6067],
+    "size": [150, 250, 150],  # Size of the dots
+    "color": ["#00ff00", "#ff0000", "#00ff00"] # Green for Online, Red for offline
+})
+
+# 2. Display the map!
+st.map(map_data, latitude="lat", longitude="lon", size="size", color="color")
--- machines.py
+++ machines.py
@@ -3,3 +3,23 @@ import pandas as pd
 
 # You may start your code here
 st.title("Machines")
+st.write("Here's the place you manage your terminal")
+
+st.button("Add New")
+
+row1 = st.columns(1)
+row2 = st.columns(1)
+row3 = st.columns(1)
+
+# Tile 1: Machine Status
+with row1[0].container(height=140):
+    st.metric(label="Machine 01", value="90%", delta="New Replace")
+    
+
+# Tile 2: Temperature Reading
+with row2[0].container(height=140):
+    # Using delta_color="inverse" makes a drop in temp green, and a rise in temp red
+    st.metric(label="Machine 02", value="87%", delta="-5%")
+
+with row3[0].container(height=140):
+    st.metric(label="Machine 02", value="30%", delta="Critical !",delta_arrow="down", delta_color="red")
